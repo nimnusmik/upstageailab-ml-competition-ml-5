0be4de2e13c591ded0a925d051108028af430d12
jkim1209 <jwk143@gmail.com>
2025-07-16 15:36:49 +0900

    feat: Add custom functions for regression

---
+++ src/data/reg_custom.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import statsmodels.api as sm
+from scipy import stats
+from sklearn.metrics import root_mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+
+def datasplit(df, Y_colname, test_size=0.2, random_state=123):
+    X_colname = [x for x in df.columns if x not in Y_colname]
+       
+    X_train, X_test, Y_train, Y_test = train_test_split(df[X_colname], df[Y_colname],
+                                                        test_size=test_size, random_state=random_state)
+    print(X_train.shape, Y_train.shape)
+    print(X_test.shape, Y_test.shape)
+    
+    return X_train, X_test, Y_train, Y_test
+
+def datasplit_ts(df, Y_colname, test_size=0.2):
+    X_colname = [x for x in df.columns if x not in Y_colname]
+
+    split_idx = int(len(df) * (1 - test_size))
+    
+    X_train = df[X_colname].iloc[:split_idx]
+    Y_train = df[Y_colname].iloc[:split_idx]
+    X_test  = df[X_colname].iloc[split_idx:]
+    Y_test  = df[Y_colname].iloc[split_idx:]
+    
+    print(X_train.shape, Y_train.shape)
+    print(X_test.shape, Y_test.shape)
+    
+    return X_train, X_test, Y_train, Y_test
+
+
+def scale(scaler, X_train, X_test):
+    scaler_fit = scaler.fit(X_train)
+    X_train_scaling = pd.DataFrame(scaler_fit.transform(X_train), 
+                                   index=X_train.index, columns=X_train.columns)
+    X_test_scaling = pd.DataFrame(scaler_fit.transform(X_test), 
+                                  index=X_test.index, columns=X_test.columns)
+    
+    return X_train_scaling, X_test_scaling
+
+
+def plot_prediction(Y_true_pred):
+    plt.figure(figsize=(16, 8))
+    plt.plot(Y_true_pred, linewidth=2, label=Y_true_pred.columns)
+    plt.xticks(fontsize=25, rotation=0)
+    plt.yticks(fontsize=25)
+    plt.xlabel('Index', fontname='serif', fontsize=28)
+    plt.legend(fontsize=20)
+    plt.grid()
+    plt.show()
+
+
+def evaluation_reg(Y_real, Y_pred):
+    MAE = mean_absolute_error(Y_real, Y_pred)
+    RMSE = root_mean_squared_error(Y_real, Y_pred)
+    MAPE = mean_absolute_percentage_error(Y_real, Y_pred)
+    Score = pd.DataFrame([RMSE, MAE, MAPE], index=['RMSE', 'MAE', 'MAPE'], columns=['Score']).T
+    
+    return Score
+
+
+def evaluation_reg_trte(Y_real_tr, Y_pred_tr, Y_real_te, Y_pred_te):
+    Score_tr = evaluation_reg(Y_real_tr, Y_pred_tr)
+    Score_te = evaluation_reg(Y_real_te, Y_pred_te)
+    Score_trte = pd.concat([Score_tr, Score_te], axis=0)
+    Score_trte.index = ['Train', 'Test']
+
+    return Score_trte
