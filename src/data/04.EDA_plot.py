91dbc0ab0dbd06622962f84cc89fe091e0416289
jkim1209 <jwk143@gmail.com>
2025-07-16 15:34:11 +0900

    refactor: Update data processing and visualization in EDA script

--- src/data/04.EDA_plot.py
+++ src/data/04.EDA_plot.py
@@ -1,5 +1,6 @@
 #%%
 # import package needed
+from IPython.display import display
 import os
 import pandas as pd
 import numpy as np
@@ -7,6 +8,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 import re
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
 
 # set kr font
 import matplotlib.font_manager
@@ -20,71 +23,82 @@ for font in krfont:
 # 마이너스 기호 깨짐 방지
 mpl.rcParams['axes.unicode_minus'] = False
 
+# 이미지 저장경로 생성
+save_dir = '../../docs/image'
+os.makedirs(save_dir, exist_ok=True)
+
+
 
 df = pd.read_csv('../../data/processed/cleaned_data/train_row_cleaned.csv')
 
-df['계약일자'] = pd.to_datetime(df['계약일자'])
-df['계약년월'] = pd.to_datetime(df['계약년월'])
-print(df.info())
+df['계약일자dt'] = pd.to_datetime(df['계약일자'])
+df['계약년월dt'] = pd.to_datetime(df['계약년월'], format='%Y%m')
+df['계약일자'] = df['계약일자dt'].dt.strftime('%Y%m%d').astype(int)
+df['계약년월'] = df['계약년월dt'].dt.strftime('%Y%m').astype(int)
+
+df.info()
 display(df.columns)
 
 
-df_cleaned = df.copy()
+
 
 
 #%%
 # 월별 평균 거래가 계산
-monthly_mean = df.groupby('계약년월')['target'].mean().reset_index()
-monthly_median = df.groupby('계약년월')['target'].median().reset_index()
+monthly_mean = df.groupby('계약년월dt')['target'].mean().reset_index()
+monthly_median = df.groupby('계약년월dt')['target'].median().reset_index()
 
 # 그래프 그리기
 plt.figure(figsize=(20, 10))
-sns.lineplot(x='계약년월', y='target', data=monthly_mean, label='평균 집값')
-sns.lineplot(x='계약년월', y='target', data=monthly_median, label='중앙값 집값')
+sns.lineplot(x='계약년월dt', y='target', data=monthly_mean, label='거래가격 평균')
+sns.lineplot(x='계약년월dt', y='target', data=monthly_median, label='거래가격 중앙값')
 
-plt.title('월별 평균 및 중앙값 집값 추이')
+plt.title('월별 거래가격 평균 및 중앙값 추이')
 plt.xlabel('계약년월')
 plt.ylabel('거래금액(만원)')
 plt.xticks(rotation=45)
 plt.grid(True)
 plt.tight_layout()
 plt.legend()
+save_path = os.path.join(save_dir, '월별 거래가격 평균 및 중앙값 추이.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
+
+
+
 
 
 #%%
-# 수치형변수 간 상관관계 확인
+# 상관관계분석
+# 브랜드등급 레이블인코딩
+le = preprocessing.LabelEncoder()
+df['브랜드등급(labeled)'] = le.fit_transform(df['브랜드등급'])
 
-num_cols = [
-    'target', '전용면적', '층', '연식', '아파트이름길이',
+cols = [
+    'target', 
+    '계약년도', '계약년월', '계약일자', 
+    '강남3구여부',
+    '전용면적', '층', '연식', '아파트이름길이', '홈페이지유무', '사용허가여부', '브랜드등급(labeled)',
     '지하철최단거리', '반경_1km_지하철역_수', '반경_500m_지하철역_수', '반경_300m_지하철역_수', 
     '버스최단거리', '반경_1km_버스정류장_수', '반경_500m_버스정류장_수', '반경_300m_버스정류장_수',
     '총인구수', '성비(남/여)',
-    '과거1개월_거래수', '과거3개월_거래수', '과거6개월_거래수', '과거12개월_거래수',
-    'loanrate_1m', 'loanrate_3m', 'loanrate_6m', 'loanrate_12m'
+    # '과거1개월_거래수', '과거3개월_거래수', '과거6개월_거래수', '과거12개월_거래수',
+    'loanrate_1m', 'loanrate_3m', 'loanrate_6m', 'loanrate_12m',
+    '좌표X', '좌표Y'
 ]
 
-corr_matrix = df_cleaned[num_cols].corr()
-
-import seaborn as sns
-import matplotlib.pyplot as plt
+corr_matrix = df[cols].corr()
 
 plt.figure(figsize=(20,15))
 sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-plt.title("수치형 변수 간 상관관계")
+plt.title("변수 간 상관관계")
+save_path = os.path.join(save_dir, '상관관계.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
-
-
-## 과거 거래수들간이나 loanrate간에 상관관계가 높은것은 당연합니다.
-## 단순히 상관계수로만 따지자면 '과거3개월_거래수' 와 'loanrate_12m'을 선택하는 것이 좋아보이나
-## 전부 넣고 나중에 회귀모델 혹은 트리모델 사용 후 변수 중요도 확인 후 가장 중요한 것 하나씩만 살리는 것이 좋을 것 같습니다.
 
 
 #%%
 # 범주형변수처리
-cat_cols = ['자치구', '법정동', '강남3구여부', '홈페이지유무', '사용허가여부', '브랜드등급']
-
-#%%
 ## 자치구별 총 거래량 확인
 dist_df = df['자치구'].value_counts().reset_index()
 dist_df.columns = ['자치구', '거래건수']
@@ -96,6 +110,8 @@ plt.title('자치구별 총 거래건수')
 plt.xticks(rotation=45)
 plt.grid(axis='y')
 plt.tight_layout()
+save_path = os.path.join(save_dir, '자치구별 총 거래건수.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
 
 
@@ -113,6 +129,8 @@ correlations.sort_values().plot(kind='barh', figsize=(10,6))
 plt.title("자치구와 거래금액간의 상관관계")
 plt.xlabel("상관계수")
 plt.grid(True)
+save_path = os.path.join(save_dir, '자치구와 거래금액간의 상관관계.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
 
 
@@ -126,18 +144,22 @@ brand= {'하이엔드': 2, '프리미엄': 1, '기타': 0}
 
 df['브랜드등급인코딩'] = df['브랜드등급'].map(brand).fillna(0).astype(int)
 
-plt.figure(figsize=(6,4))
-sns.boxplot(data=df, x='브랜드등급', y='target', order=['기타', '프리미엄', '하이엔드'])
-plt.title("브랜드등급별 거래가격 분포")
-plt.ylabel("거래가격") 
-plt.grid(axis='y')
-plt.show()
+# plt.figure(figsize=(6,4))
+# sns.boxplot(data=df, x='브랜드등급', y='target', order=['기타', '프리미엄', '하이엔드'])
+# plt.title("브랜드등급별 거래가격 분포")
+# plt.ylabel("거래가격") 
+# plt.grid(axis='y')
+# save_path = os.path.join(save_dir, '브랜드등급별 거래가격 분포.png')
+# plt.savefig(save_path, bbox_inches='tight', dpi=300)
+# plt.show()
 
 plt.figure(figsize=(6,4))
 sns.boxplot(data=df, x='브랜드등급', y=np.log1p(df['target']), order=['기타', '프리미엄', '하이엔드'])
 plt.title("브랜드등급별 log(거래가격) 분포")
 plt.ylabel("log(거래가격)") 
 plt.grid(axis='y')
+save_path = os.path.join(save_dir, '브랜드등급별 log(거래가격) 분포.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
 
 
@@ -147,14 +169,16 @@ brand_df = df['강남3구여부'].value_counts().reset_index()
 brand_df.columns = ['강남3구여부', '거래건수']
 display(brand_df)
 
-plt.figure(figsize=(6,4))
-sns.boxplot(data=df, x='강남3구여부', y='target')
-plt.title('강남3구 여부에 따른 거래금액 분포')
-plt.xlabel('강남3구 여부')
-plt.ylabel('거래가격')
-plt.grid(axis='y')
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(6,4))
+# sns.boxplot(data=df, x='강남3구여부', y='target')
+# plt.title('강남3구 여부에 따른 거래금액 분포')
+# plt.xlabel('강남3구 여부')
+# plt.ylabel('거래가격')
+# plt.grid(axis='y')
+# plt.tight_layout()
+# save_path = os.path.join(save_dir, '강남3구(강남, 서초, 송파) 여부에 따른 거래금액 분포.png')
+# plt.savefig(save_path, bbox_inches='tight', dpi=300)
+# plt.show()
 
 plt.figure(figsize=(6,4))
 sns.boxplot(data=df, x='강남3구여부', y=np.log1p(df['target']))
@@ -163,6 +187,8 @@ plt.xlabel('강남3구 여부')
 plt.ylabel('log(거래가격)')
 plt.grid(axis='y')
 plt.tight_layout()
+save_path = os.path.join(save_dir, '강남3구(강남, 서초, 송파) 여부에 따른 log(거래금액) 분포.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
 
 #%%
@@ -171,14 +197,16 @@ brand_df = df['홈페이지유무'].value_counts().reset_index()
 brand_df.columns = ['홈페이지유무', '거래건수']
 display(brand_df)
 
-plt.figure(figsize=(6,4))
-sns.boxplot(data=df, x='홈페이지유무', y='target')
-plt.title('홈페이지유무에 따른 거래금액 분포')
-plt.xlabel('홈페이지유무')
-plt.ylabel('거래가격')
-plt.grid(axis='y')
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(6,4))
+# sns.boxplot(data=df, x='홈페이지유무', y='target')
+# plt.title('홈페이지유무에 따른 거래금액 분포')
+# plt.xlabel('홈페이지유무')
+# plt.ylabel('거래가격')
+# plt.grid(axis='y')
+# plt.tight_layout()
+# save_path = os.path.join(save_dir, '홈페이지유무에 따른 거래금액 분포.png')
+# plt.savefig(save_path, bbox_inches='tight', dpi=300)
+# plt.show()
 
 plt.figure(figsize=(6,4))
 sns.boxplot(data=df, x='홈페이지유무', y=np.log1p(df['target']))
@@ -187,6 +215,8 @@ plt.xlabel('홈페이지유무')
 plt.ylabel('log(거래가격)')
 plt.grid(axis='y')
 plt.tight_layout()
+save_path = os.path.join(save_dir, '홈페이지유무에 따른 log(거래금액) 분포.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
 
 
@@ -196,14 +226,16 @@ brand_df = df['사용허가여부'].value_counts().reset_index()
 brand_df.columns = ['사용허가여부', '거래건수']
 display(brand_df)
 
-plt.figure(figsize=(6,4))
-sns.boxplot(data=df, x='사용허가여부', y='target')
-plt.title('사용허가여부에 따른 거래금액 분포')
-plt.xlabel('사용허가여부')
-plt.ylabel('거래가격')
-plt.grid(axis='y')
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(6,4))
+# sns.boxplot(data=df, x='사용허가여부', y='target')
+# plt.title('사용허가여부에 따른 거래금액 분포')
+# plt.xlabel('사용허가여부')
+# plt.ylabel('거래가격')
+# plt.grid(axis='y')
+# plt.tight_layout()
+# save_path = os.path.join(save_dir, '사용허가여부에 따른 거래금액 분포.png')
+# plt.savefig(save_path, bbox_inches='tight', dpi=300)
+# plt.show()
 
 plt.figure(figsize=(6,4))
 sns.boxplot(data=df, x='사용허가여부', y=np.log1p(df['target']))
@@ -212,13 +244,15 @@ plt.xlabel('사용허가여부')
 plt.ylabel('log(거래가격)')
 plt.grid(axis='y')
 plt.tight_layout()
+save_path = os.path.join(save_dir, '사용허가여부에 따른 log(거래금액) 분포.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
 
 
 #%%
 # 자치구별 시기별 거래건수
 plt.figure(figsize=(18, 10))
-sns.lineplot(data=df.groupby(['계약년월', '자치구']).size().reset_index(name='거래건수'), x='계약년월', y='거래건수', hue='자치구', palette='Set2')
+sns.lineplot(data=df.groupby(['계약년월dt', '자치구']).size().reset_index(name='거래건수'), x='계약년월dt', y='거래건수', hue='자치구', palette='Set2')
 plt.title("자치구별 월별 거래건수")
 plt.xlabel("계약년월")
 plt.ylabel("거래건수")
@@ -226,6 +260,8 @@ plt.xticks(rotation=45)
 plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.grid(True)
+save_path = os.path.join(save_dir, '자치구별 월별 거래건수.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
 
 plt.figure(figsize=(18, 10))
@@ -237,12 +273,15 @@ plt.xticks(rotation=45)
 plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.grid(True)
+save_path = os.path.join(save_dir, '자치구별 년도별 거래건수.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
+
 
 #%%
 # 자치구별 시기별 거래액수 평균
 plt.figure(figsize=(18, 10))
-sns.lineplot(data=df.groupby(['계약년월', '자치구'])['target'].mean().reset_index(), x='계약년월', y='target', hue='자치구', palette='Set2')
+sns.lineplot(data=df.groupby(['계약년월dt', '자치구'])['target'].mean().reset_index(), x='계약년월dt', y='target', hue='자치구', palette='Set2')
 plt.title("자치구별 월별 거래가격 평균")
 plt.xlabel("계약년월")
 plt.ylabel("거래가격(만원)")
@@ -250,6 +289,8 @@ plt.xticks(rotation=45)
 plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.grid(True)
+save_path = os.path.join(save_dir, '자치구별 월별 거래가격 평균.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
 
 plt.figure(figsize=(18, 10))
@@ -261,20 +302,23 @@ plt.xticks(rotation=45)
 plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.grid(True)
+save_path = os.path.join(save_dir, '자치구별 년도별 거래가격 평균.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
+
 
 #%%
 # 자치구별 시기별 거래액수 중앙값
-plt.figure(figsize=(18, 10))
-sns.lineplot(data=df.groupby(['계약년월', '자치구'])['target'].median().reset_index(), x='계약년월', y='target', hue='자치구', palette='Set2')
-plt.title("자치구별 월별 거래가격 중앙값")
-plt.xlabel("계약년월")
-plt.ylabel("거래가격(만원)")
-plt.xticks(rotation=45)
-plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.tight_layout()
-plt.grid(True)
-plt.show()
+# plt.figure(figsize=(18, 10))
+# sns.lineplot(data=df.groupby(['계약년월dt', '자치구'])['target'].median().reset_index(), x='계약년월dt', y='target', hue='자치구', palette='Set2')
+# plt.title("자치구별 월별 거래가격 중앙값")
+# plt.xlabel("계약년월")
+# plt.ylabel("거래가격(만원)")
+# plt.xticks(rotation=45)
+# plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
+# plt.tight_layout()
+# plt.grid(True)
+# plt.show()
 
 plt.figure(figsize=(18, 10))
 sns.lineplot(data=df.groupby(['계약년도', '자치구'])['target'].median().reset_index(), x='계약년도', y='target', hue='자치구', palette='Set2')
@@ -285,20 +329,23 @@ plt.xticks(rotation=45)
 plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.grid(True)
+save_path = os.path.join(save_dir, '자치구별 년도별 거래가격 중앙값.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
+
 
 #%%
 # 자치구별 시기별 거래액수 최소값
-plt.figure(figsize=(18, 10))
-sns.lineplot(data=df.groupby(['계약년월', '자치구'])['target'].min().reset_index(), x='계약년월', y='target', hue='자치구', palette='Set2')
-plt.title("자치구별 월별 거래가격 최소값")
-plt.xlabel("계약년월")
-plt.ylabel("거래가격(만원)")
-plt.xticks(rotation=45)
-plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.tight_layout()
-plt.grid(True)
-plt.show()
+# plt.figure(figsize=(18, 10))
+# sns.lineplot(data=df.groupby(['계약년월dt', '자치구'])['target'].min().reset_index(), x='계약년월dt', y='target', hue='자치구', palette='Set2')
+# plt.title("자치구별 월별 거래가격 최소값")
+# plt.xlabel("계약년월")
+# plt.ylabel("거래가격(만원)")
+# plt.xticks(rotation=45)
+# plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
+# plt.tight_layout()
+# plt.grid(True)
+# plt.show()
 
 plt.figure(figsize=(18, 10))
 sns.lineplot(data=df.groupby(['계약년도', '자치구'])['target'].min().reset_index(), x='계약년도', y='target', hue='자치구', palette='Set2')
@@ -309,20 +356,23 @@ plt.xticks(rotation=45)
 plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.grid(True)
+save_path = os.path.join(save_dir, '자치구별 년도별 거래가격 최소값.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
+
 
 #%%
 # 자치구별 시기별 거래액수 최대값
-plt.figure(figsize=(18, 10))
-sns.lineplot(data=df.groupby(['계약년월', '자치구'])['target'].max().reset_index(), x='계약년월', y='target', hue='자치구', palette='Set2')
-plt.title("자치구별 월별 거래가격 최대값")
-plt.xlabel("계약년월")
-plt.ylabel("거래가격(만원)")
-plt.xticks(rotation=45)
-plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.tight_layout()
-plt.grid(True)
-plt.show()
+# plt.figure(figsize=(18, 10))
+# sns.lineplot(data=df.groupby(['계약년월dt', '자치구'])['target'].max().reset_index(), x='계약년월dt', y='target', hue='자치구', palette='Set2')
+# plt.title("자치구별 월별 거래가격 최대값")
+# plt.xlabel("계약년월")
+# plt.ylabel("거래가격(만원)")
+# plt.xticks(rotation=45)
+# plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
+# plt.tight_layout()
+# plt.grid(True)
+# plt.show()
 
 plt.figure(figsize=(18, 10))
 sns.lineplot(data=df.groupby(['계약년도', '자치구'])['target'].max().reset_index(), x='계약년도', y='target', hue='자치구', palette='Set2')
@@ -333,5 +383,7 @@ plt.xticks(rotation=45)
 plt.legend(title='자치구', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.grid(True)
+save_path = os.path.join(save_dir, '자치구별 년도별 거래가격 최대값.png')
+plt.savefig(save_path, bbox_inches='tight', dpi=300)
 plt.show()
 
